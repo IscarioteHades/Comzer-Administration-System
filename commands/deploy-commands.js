698f1ede7898230e79c52d5488931b39124c3e91
IscarioteHades <124427496+IscarioteHades@users.noreply.github.com>
2025-07-10 00:41:03 +0900

    Update deploy-commands.js

--- commands/deploy-commands.js
+++ commands/deploy-commands.js
@@ -2,6 +2,12 @@ import { REST, Routes } from 'discord.js';
 import { data as rolepost } from './embedPost.js';
 import config from '../config.json' with { type: 'json' };
 
+/* ▼ 追加：ビルド時（トークン無）ならスキップして正常終了 */
+if (!process.env.DISCORD_TOKEN) {
+  console.log('DISCORD_TOKEN not set - skip slash-command deploy');
+  process.exit(0);
+}
+
 const rest = new REST().setToken(process.env.DISCORD_TOKEN);
 
 try {

61e2c1f5ddeb65ff363bacbfbd646a62c5711a7a
IscarioteHades <124427496+IscarioteHades@users.noreply.github.com>
2025-07-12 13:49:24 +0900

    Update deploy-commands.js

--- commands/deploy-commands.js
+++ commands/deploy-commands.js
@@ -1,12 +1,7 @@
 import { REST, Routes } from 'discord.js';
 import { data as rolepost } from './embedPost.js';
+import { data as status } from './status.js';
 import config from '../config.json' with { type: 'json' };
-
-/* ▼ 追加：ビルド時（トークン無）ならスキップして正常終了 */
-if (!process.env.DISCORD_TOKEN) {
-  console.log('DISCORD_TOKEN not set - skip slash-command deploy');
-  process.exit(0);
-}
 
 const rest = new REST().setToken(process.env.DISCORD_TOKEN);
 
@@ -14,7 +9,7 @@ try {
   console.log('🔄  スラッシュコマンドを登録中...');
   await rest.put(
     Routes.applicationGuildCommands(config.clientId, config.guildId),
-    { body: [rolepost.toJSON()] },
+    { body: [rolepost.toJSON(), status.toJSON()] },
   );
   console.log('✅  登録完了！');
 } catch (err) {

a7c1bac857e159764a7b97ad640e9233e5ad3271
IscarioteHades <124427496+IscarioteHades@users.noreply.github.com>
2025-07-13 16:36:52 +0900

    Update deploy-commands.js

--- commands/deploy-commands.js
+++ commands/deploy-commands.js
@@ -2,7 +2,8 @@ import { REST, Routes } from 'discord.js';
 import { data as rolepost } from './embedPost.js';
 import { data as status } from './status.js';
 import config from '../config.json' with { type: 'json' };
-import { data as deleteRolepost } from './blacklistCommands.js';
+import { data as deleteRolepost } from '../blacklistCommands.js';
+
 
 const rest = new REST().setToken(process.env.DISCORD_TOKEN);
 

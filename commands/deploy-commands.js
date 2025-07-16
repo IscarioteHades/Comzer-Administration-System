0ad332af1659f424683c260a624d2f4330ecae39
take0819 <sg4053747100@stu.shizugaku.ed.jp>
2025-07-17 08:21:32 +0900

    Update deploy-commands.js

--- commands/deploy-commands.js
+++ commands/deploy-commands.js
@@ -1,3 +1,4 @@
+import { Client, GatewayIntentBits, Events, Collection } from 'discord.js'
 import { REST, Routes } from 'discord.js';
 import fs from 'fs';
 import path from 'path';
@@ -6,24 +7,21 @@ import { commands as blacklistCommands } from '../blacklistCommands.js';
 
 
 const rest = new REST().setToken(process.env.DISCORD_TOKEN);
-
 try {
   console.log('🔄  スラッシュコマンドを登録中...');
-  const commandsDir = path.resolve('./commands');
-  const dynamicCommands = fs
-    .readdirSync(commandsDir)
-    .filter(f => f.endsWith('.js'))
-    .map(f => {
-      const { data } = await import(path.join(commandsDir, f));
-      return data.toJSON();
-    });
-  const blCommandJson = blacklistCommands.map(c => c.data.toJSON());
+  const commandsPath = path.resolve('./commands');
+  const dynamic = fs.readdirSync(commandsPath)
+  .filter(f => f.endsWith('.js'))
+  .map(f => {
+  const { data } = require(path.join(commandsPath, f));
+  return data.toJSON();
+});
+  const blJson = blCommands.map(c => c.data.toJSON());
   await rest.put(
-    Routes.applicationGuildCommands(config.clientId, config.guildId),
-    { body: [...dynamicCommands, ...blCommandJson] },
-  );
-  console.log('✅  登録完了！');
+  Routes.applicationGuildCommands(config.clientId, config.guildId),
+  { body: [...dynamic, ...blJson] }
+);
+  console.log('✅ スラッシュコマンド登録完了！');
 } catch (err) {
-  console.error(err);
+  console.error('❌ コマンド登録エラー:', err);
 }
-process.exit(0);

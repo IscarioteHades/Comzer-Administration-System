5f128f39df25d0ccc1a8a92a8560da2841b68824
take0819 <sg4053747100@stu.shizugaku.ed.jp>
2025-07-17 08:42:22 +0900

    Update deploy-commands.js

--- commands/deploy-commands.js
+++ commands/deploy-commands.js
@@ -1,26 +1,24 @@
 import { REST, Routes } from 'discord.js';
-import fs from 'fs';
-import path from 'path';
+import { data as rolepost } from './embedPost.js';
+import { data as status } from './status.js';
 import config from '../config.json' with { type: 'json' };
 import { commands as blacklistCommands } from '../blacklistCommands.js';
 
+
 const rest = new REST().setToken(process.env.DISCORD_TOKEN);
+
 try {
   console.log('🔄  スラッシュコマンドを登録中...');
-  const commandsPath = path.resolve('./commands');
-  const dynamic = fs
-    .readdirSync(commandsPath)
-    .filter(f => f.endsWith('.js') && f !== 'deploy-commands.js')
-    .map(f => {
-      const mod = require(path.join(commandsPath, f));
-      return mod.data.toJSON();
-    });
-  const blJson = blacklistCommands.map(c => c.toJSON());
   await rest.put(
   Routes.applicationGuildCommands(config.clientId, config.guildId),
-  { body: [...dynamic, ...blJson] }
+  { body: [rolepost.toJSON(), status.toJSON(), ...blacklistCommands.map(c => c.toJSON())] },
 );
-  console.log('✅ スラッシュコマンド登録完了！');
+  console.log('✅  登録完了！');
 } catch (err) {
-  console.error('❌ コマンド登録エラー:', err);
+  console.error(err);
 }
+await rest.put(
+  Routes.applicationGuildCommands(config.clientId, config.guildId),
+  { body: [rolepost.toJSON(), status.toJSON(), ...blacklistCommands.map(c => c.toJSON())] },
+);
+process.exit(0);

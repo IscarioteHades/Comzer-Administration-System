4d25fad03d6556e75ac2e5f814027feaf89fedbd
take0819 <sg4053747100@stu.shizugaku.ed.jp>
2025-09-01 15:33:18 +0900

    Update index.js

--- index.js
+++ index.js
@@ -313,6 +313,7 @@ bot.commands = new Collection([
   [statusCommand.data.name, statusCommand],
   [shutdownData.name,       { data: shutdownData, execute: shutdownExec }],
 ]);
+require('./lokiForwarder').start();
 // ── Botがログインして準備完了したら一度だけblacklistCommands.js側を初期化
 bot.once("ready", async () => {
   console.log(`Logged in as ${bot.user.tag} | initializing blacklist…`);
